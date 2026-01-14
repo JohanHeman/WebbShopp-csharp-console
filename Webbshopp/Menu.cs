03f02cba7102041dde6e26d1e661a44ed749225e
Johan Heman <jojo2001@hotmail.se>
2026-01-14 06:09:37 +0100

    Add error management and loops to the menus

--- Webbshopp/Menu.cs
+++ Webbshopp/Menu.cs
@@ -22,71 +22,103 @@ namespace Webbshop
             
         public static void StartMenu()
         {
-            // later in this the names of the books will be from the database, isDeal= true
-            Console.Clear();
-            List<string> welcomeText = new List<string> { "Welcome to the Book store. ", "We provide books" };
-
-            Window windowStart = new Window("The Book Shop", 37, 0, welcomeText);
-            windowStart.Draw();
-
-            List<string> dealOne = new List<string> { "Book name 1", "Author", "Press p to purchase" };
-            var dealOneWindow = new Window("Deal 1", 10, 5, dealOne);
-            dealOneWindow.Draw();
-
-            List<string> dealTwo = new List<string> { "Book name 2", "Author", "Press p to purchase" };
-            var dealTwoWindow = new Window("Deal 2", 40, 5, dealTwo);
-            dealTwoWindow.Draw();
-
-            List<string> dealThree = new List<string> { "Book name 3", "Author", "Press p to purchase" };
-            var dealThreeWindow = new Window("Deal 3", 70, 5, dealThree);
-            dealThreeWindow.Draw();
-
-            List<string> menuChoices = Helpers.EnumsToLists(typeof(Enums.HomeEnums));
-
-            var menuWindow = new Window("Menu", 2, 0, menuChoices);
-            menuWindow.Draw();
-
-            ConsoleKeyInfo key = Console.ReadKey(true);
-            if(int.TryParse(key.KeyChar.ToString(), out int input))
+            while (true)
             {
-                switch ((Enums.HomeEnums)input)
-                {
-                    case Enums.HomeEnums.Customer_menu:
-                        CustomerMenu();
-                        break;
-                    case Enums.HomeEnums.Admin_menu:
-                        AdminMenu();
-                        break;
+                // later in this the names of the books will be from the database, isDeal= true
+                    Console.Clear();
+                List<string> welcomeText = new List<string> { "Welcome to the Book store. ", "We provide books" };
 
+                Window windowStart = new Window("The Book Shop", 37, 0, welcomeText);
+                windowStart.Draw();
+
+                List<string> dealOne = new List<string> { "Book name 1", "Author", "Press p to purchase" };
+                var dealOneWindow = new Window("Deal 1", 10, 5, dealOne);
+                dealOneWindow.Draw();
+
+                List<string> dealTwo = new List<string> { "Book name 2", "Author", "Press p to purchase" };
+                var dealTwoWindow = new Window("Deal 2", 40, 5, dealTwo);
+                dealTwoWindow.Draw();
+
+                List<string> dealThree = new List<string> { "Book name 3", "Author", "Press p to purchase" };
+                var dealThreeWindow = new Window("Deal 3", 70, 5, dealThree);
+                dealThreeWindow.Draw();
+
+                List<string> menuChoices = Helpers.EnumsToLists(typeof(Enums.HomeEnums));
+
+                var menuWindow = new Window("Menu", 2, 0, menuChoices);
+                menuWindow.Draw();
+
+                ConsoleKeyInfo key = Console.ReadKey(true);
+
+                if (key.KeyChar == 'q') break;
+
+                else
+                {
+                    if (int.TryParse(key.KeyChar.ToString(), out int input))
+                    {
+
+                        switch ((Enums.HomeEnums)input)
+                        {
+                            case Enums.HomeEnums.Customer_menu:
+                                CustomerMenu();
+                                break;
+                            case Enums.HomeEnums.Admin_menu:
+                                AdminMenu();
+                                break;
+
+                        }
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Not a valid input please try again");
+                        Console.WriteLine("Press any key to go back");
+
+                        Console.ReadKey(true);
+                    }
                 }
             }
+            Console.Clear();
+            Console.WriteLine("Thank you for visiting. Please come again!");
+
         }
-
-
-
 
         public static void CustomerMenu()
         {
-            Console.Clear();
-            List<string> customerChoices = Helpers.EnumsToLists(typeof(Enums.customerEnums));
-
-            var window = new Window("CustomerMenu", 2, 0, customerChoices);
-            window.Draw();
-
-            ConsoleKeyInfo key = Console.ReadKey(true);
-
-            if(int.TryParse(key.KeyChar.ToString(), out int input))
+            while(true)
             {
-                switch((Enums.customerEnums)input)
+                Console.Clear();
+                List<string> customerChoices = Helpers.EnumsToLists(typeof(Enums.customerEnums));
+
+                var window = new Window("CustomerMenu", 2, 0, customerChoices);
+                window.Draw();
+
+                ConsoleKeyInfo key = Console.ReadKey(true);
+
+                if (int.TryParse(key.KeyChar.ToString(), out int input))
                 {
-                    case Enums.customerEnums.Home:
-                        StartMenu();
-                        break;
-                    case Enums.customerEnums.Shop:
-                        ShopMenu();
-                        break;
+                    switch ((Enums.customerEnums)input)
+                    {
+                        case Enums.customerEnums.Home:
+                            Console.WriteLine("Returning to home...");
+                            Thread.Sleep(1000);
+                            return;
+                        case Enums.customerEnums.Shop:
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Not a valid input please try again");
+                    Console.WriteLine("Press any key to go back");
+
+                    Console.ReadKey(true);
                 }
             }
+
+
+
         }
 
 
@@ -94,32 +126,45 @@ namespace Webbshop
 
         public static void AdminMenu()
         {
-            Console.Clear();
-            List<string> adminChoices = Helpers.EnumsToLists(typeof(Enums.adminEnums));
-
-            var window = new Window("AdminMenu", 2, 0, adminChoices);
-            window.Draw();
-
-            ConsoleKeyInfo key = Console.ReadKey(true);
-            if(int.TryParse(key.KeyChar.ToString(), out int input))
+            while(true)
             {
-                switch((Enums.adminEnums) input)
-                {
-                    case Enums.adminEnums.Product_management:
-                        ProductAdmin();
-                        break;
-                    case Enums.adminEnums.Product_categories:
-                        ProductCategories();
-                        break;
+                Console.Clear();
+                List<string> adminChoices = Helpers.EnumsToLists(typeof(Enums.adminEnums));
 
-                    case Enums.adminEnums.Customer_management:
-                        AdminCustomer();
-                        break;
-                    case Enums.adminEnums.Home:
-                        StartMenu();
-                        break;
+                var window = new Window("AdminMenu", 2, 0, adminChoices);
+                window.Draw();
+
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                if (int.TryParse(key.KeyChar.ToString(), out int input))
+                {
+                    switch ((Enums.adminEnums)input)
+                    {
+                        case Enums.adminEnums.Product_management:
+                            ProductAdmin();
+                            break;
+                        case Enums.adminEnums.Product_categories:
+                            ProductCategories();
+                            break;
+
+                        case Enums.adminEnums.Customer_management:
+                            AdminCustomer();
+                            break;
+                        case Enums.adminEnums.Home:
+                            Console.WriteLine("Returning to home...");
+                            Thread.Sleep(1000);
+                            return;
+                    }
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Not a valid input please try again");
+                    Console.WriteLine("Press any key to go back");
+
+                    Console.ReadKey(true);
                 }
             }
+
         }
 
 
@@ -132,6 +177,10 @@ namespace Webbshop
 
             var window = new Window("AdminMenu", 2, 0, productList);
             window.Draw();
+
+            // input for the enums here
+
+            Console.ReadKey(true);
         }
 
 
@@ -144,6 +193,8 @@ namespace Webbshop
 
             var window = new Window("AdminMenu", 2, 0, categoryList); // admin will be able to filter bu categories when looking for items.
             window.Draw();
+
+            Console.ReadKey(true);
         }
 
 
@@ -190,7 +241,6 @@ namespace Webbshop
             var window = new Window("Shop",2, 0, shopList);
 
             window.Draw();
-
 
             ConsoleKeyInfo key = Console.ReadKey(true);
 
